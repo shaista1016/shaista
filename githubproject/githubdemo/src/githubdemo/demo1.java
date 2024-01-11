68703d7536b9d1a098d707059547012393ab6ad0
Shaista <shai.khan1016@gmail.com>
2024-01-11 09:04:11 +0530

    first commit

---
+++ githubproject/githubdemo/src/githubdemo/demo1.java
@@ -0,0 +1,12 @@
+package githubdemo;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Github Hello World");
+	}
+void display() {
+	System.out.println("Branch");
+}
+}
