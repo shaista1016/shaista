940ce9c6be836d60e8201afda97f380addd24413
Shaista <shai.khan1016@gmail.com>
2024-01-18 09:43:18 +0530

    commit statement

---
+++ githubproject/githubdemo/src/githubdemo/addno.java
@@ -0,0 +1,16 @@
+package githubdemo;
+
+public class addno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=8;
+int y=8;
+int sum=x+y;
+System.out.println("The sum is :"+sum);
+		
+		
+		
+	}
+
+}
