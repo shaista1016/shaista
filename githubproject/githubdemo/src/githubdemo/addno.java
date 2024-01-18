9e571417aa36f80a8ac855d1e423df87c1063cb5
Shaista <shai.khan1016@gmail.com>
2024-01-18 10:18:19 +0530

    commit

--- githubproject/githubdemo/src/githubdemo/addno.java
+++ githubproject/githubdemo/src/githubdemo/addno.java
@@ -1,16 +1,27 @@
 package githubdemo;
 
 public class addno {
+	static void subtract() {
+		int x=12;
+		int y=6;
+		int sub= x-y;
+		System.out.println("The difference  is :"+sub);
+		
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 int x=8;
 int y=8;
 int sum=x+y;
 System.out.println("The sum is :"+sum);
 		
-		
+subtract();
+
 		
 	}
-
+	
+	
 }
